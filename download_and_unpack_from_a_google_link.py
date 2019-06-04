541d807bb7bf00b5aa08aea23c48aecbb464aec2
EC2 Default User <ec2-user@ip-172-31-39-82.ec2.internal>
2019-06-04 15:03:31 +0000

    download_and_unpack script changed

--- download_and_unpack_from_a_google_link.py
+++ download_and_unpack_from_a_google_link.py
@@ -1,14 +1,24 @@
 import os
 
+def make_sure_path_to_file_exists(path):
+  folder = os.path.dirname(path)
+  try:
+    os.makedirs(folder)
+  except:
+    print("all good, ", folder, " already exists")
+  return 
+
+
 def download_small_gfile_from_id(file_id, output_file_with_path):
   make_sure_path_to_file_exists(output_file_with_path)
-  output_file_with_path = wget.download('https://docs.google.com/uc?export=download&id=' + file_id)
   ## WGET ##
-  os.system("wget -O " + $output_file_with_path + ' https://docs.google.com/uc?export=download&id=' + file_id)
+  command = "sudo wget -O " + output_file_with_path + ' https://docs.google.com/uc?export=download&id=' + file_id
+  print(command)
+  os.system(command)
 
 #example for file_link = 'https://drive.google.com/open?id=1vFsgvRaLrMzXPwxx45iGz7vMjiQa9Uuy'
 file_id = '1vFsgvRaLrMzXPwxx45iGz7vMjiQa9Uuy'
-filename = '/content/tmp.txt'
+filename = 'test.txt'
 
-file_link = input("provide link to .env file from google drive ")
+file_id = input("provide link to .env file from google drive ")
 download_small_gfile_from_id(file_id, filename)
